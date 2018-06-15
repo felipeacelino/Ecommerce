ca06dd15b118b519b84d8262dd78535c3245a94d
Felipe Acelino <felipeacelino@hotmail.com>
2018-06-14 23:28:38 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -106,6 +106,27 @@ class Category extends Model {
 
     }
 
+    public function getProductsPage($page = 1, $itemsPerPage = 8) {
+
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS * FROM tb_products a INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct INNER JOIN tb_categories c ON c.idcategory = b.idcategory WHERE c.idcategory = :idcategory LIMIT $start, $itemsPerPage", array(
+            ":idcategory" => $this->getidcategory(),
+        ));
+
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal");
+        $resultTotal = (int)$resultTotal[0]['nrtotal'];
+
+        return array(
+            "data" => Product::checkList($results),
+            "total" => $resultTotal,
+            "pages" => ceil($resultTotal / $itemsPerPage),
+        );
+
+    }
+
     public function addProduct(Product $product) {
         
         $sql = new Sql();
