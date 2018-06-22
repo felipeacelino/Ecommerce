34b55191bc5d00549a7f21f7c4ce0f1fccf530a9
Felipe Acelino <felipeacelino@hotmail.com>
2018-06-21 22:48:45 -0300

    Usuários - Paginação e busca

--- admin-users.php
+++ admin-users.php
@@ -7,12 +7,36 @@ $app->get('/admin/users', function() {
 
 	User::verifyLogin();
 
-	$users = User::listAll();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if ($search != "") {
+	
+		$pagination = User::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = User::getPage($page);
+
+	}
+
+	$pages = array();
+
+	for ($x=1; $x<=$pagination['pages']; $x++) {
+		array_push($pages, array(
+			"href" => "http://localhost/ecommerce/admin/users?".http_build_query(array(
+				"page" => $x,
+				"search" => $search
+			)),
+			"text" => $x
+		));
+	}
 
 	$page = new PageAdmin();
-
 	$page->setTpl("users", array(
-		'users' => $users
+		'users' => $pagination['data'],
+		'search' => $search,
+		'pages' => $pages
 	));
 
 });
